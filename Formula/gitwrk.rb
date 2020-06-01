db9b1f3d2684f3f508354e313bfcdebfc4c89221
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-01 16:14:58 +0200

    Brew formula update for gitwrk version v1.0.5

--- Formula/gitwrk.rb
+++ Formula/gitwrk.rb
@@ -2,16 +2,16 @@
 class Gitwrk < Formula
   desc "A small CLI tool that helps you with monthly reports by extracting working hours directly from git repository."
   homepage "https://github.com/unravela/gitwrk"
-  version "1.0.4"
+  version "1.0.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/unravela/gitwrk/releases/download/v1.0.4/gitwrk_1.0.4_Darwin_x86_64.tar.gz"
-    sha256 "8980441c49e1790547b4c7ff3e3180be5718138585245b4ab50557d69da26b53"
+    url "https://github.com/unravela/gitwrk/releases/download/v1.0.5/gitwrk_1.0.5_Darwin_x86_64.tar.gz"
+    sha256 "588aba68802c59d0acec46e699af3574ce70932db14052dbea8677604a07604a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/unravela/gitwrk/releases/download/v1.0.4/gitwrk_1.0.4_Linux_x86_64.tar.gz"
-      sha256 "fa640ffad0fd908faceb058e12992904bfe98d99bb7938e05e8f0762cc02cb8d"
+      url "https://github.com/unravela/gitwrk/releases/download/v1.0.5/gitwrk_1.0.5_Linux_x86_64.tar.gz"
+      sha256 "5339dd9632218fdd016e8e830165e291420f7abc9e60cc742bd6b873c4f1d4b4"
     end
   end
 
